ca9c0bde7fd38b3175442955fd56dd9e06d78f11
awRosx <hjcrji996@gmail.com>
2022-08-29 11:32:52 +0300

    training commit

---
+++ mathFunc.php
@@ -0,0 +1,12 @@
+<?php
+    function math($a, $b, $c)
+    {
+        $k = (100 * 109.24 * $a) / ($b - $c) ** 3;
+        if ( $a < 0 or $b < 0 or $c < 0 or $b < $c){
+            return null;
+        }
+        else{
+            return $k;
+        }
+    }
+echo math(16, 18, 17);
